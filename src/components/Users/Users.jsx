0912864db7c03618f59f8bd44978c1913414a7bb
Ilya Gribov <iagribov@avito.ru>
2020-04-26 17:13:06 +0300

    fix-1

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cn from "classnames";
 import s from './Users.module.css'
 import userPhoto from '../../assets/images/images.png'
 
@@ -6,29 +7,39 @@ const Users = (props) => {
 
     let pageCount = Math.ceil(props.totalUsersCount / props.pageSize)
 
-    let pages = []
-    for (let i = 1; i <= pageCount; i++) {
-        pages.push(i)
+    const handlePageClick = e => {
+        const { pageId } = e.target.dataset;
+        e.preventDefault();
+        console.log(pageId);
+        pageId && props.onPageChanged(+pageId);
     }
+    let pages = [];
+
+    for (let i = 1; i <= pageCount; i++) {
+        pages.push(
+            <span
+                key={`page_${i}`}
+                data-page-id={i}
+                className={cn(s.page, {[s.selectPage]: props.currentPage === i })}
+                onClick={handlePageClick}>
+                {i}
+            </span>
+        );
+    }
+
     return <div>
         <div>
-            {pages.map(p => {
-                return <span className={props.currentPage === p && s.selectPage} onClick={() => {
-                    props.onPageChanged(p)
-                }}>
-                        {p}</span>
-            })}
-
+            {pages}
         </div>
 
         {props.users.map(u => <div key={u.id}>
             <div className={s.users}>
-                <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} />
             </div>
             <div>
                 {u.followed ? <button onClick={() => {
-                        props.follow(u.id)
-                    }}>Follow</button>
+                    props.follow(u.id)
+                }}>Follow</button>
                     : <button onClick={() => {
                         props.unFollow(u.id)
                     }}>Unfollow</button>}
